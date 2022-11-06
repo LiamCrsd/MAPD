ad816fd405d881736000efe3e4ae87a985d3c293
LiamCrsd <liam.coursodon@gmail.com>
2022-11-06 18:11:46 +0100

    ajou test

---
+++ test/PetriNetTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PetriNetTest {
+
+	@Test
+	void test() {
+		PetriNet test = new PetriNet();
+		test.affichage();
+		fail("Not yet implemented");
+	}
+
+}
